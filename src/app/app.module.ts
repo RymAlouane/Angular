1f80ad31f765acfb4bcc2cdd0233ab02518670a8
[alouaneR] <[rym.alouane@esprit.tn]>
2022-10-22 15:38:28 +0100

    22/10Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListProductComponent } from './list-product/list-product.component';
 import { FooterComponent } from './footer/footer.component';
 import {FormsModule} from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ListProductComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
